1dd6b9510d1a925a13736c5b65edb6a9bce03c90
Han Pham <41082484+hanpm@users.noreply.github.com>
2024-03-18 23:37:50 -0700

    adding js counter function + editing html

---
+++ index.js
@@ -0,0 +1,8 @@
+// Calling the lambda function 
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://dafbprumukerwlot2pqi3jg7o40orqxd.lambda-url.us-west-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data};`
+}
+updateCounter();
