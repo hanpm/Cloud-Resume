774220479824fceeeadd4210b2a2680e1e9f0a2f
Han Pham <41082484+hanpm@users.noreply.github.com>
2024-03-19 00:44:30 -0700

    Getting the counts from the json response

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://dafbprumukerwlot2pqi3jg7o40orqxd.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `${data}`;
+    console.log(data);
+    counter.innerHTML = `${data['Item']['view_counts']}`;
 }
 updateCounter();
