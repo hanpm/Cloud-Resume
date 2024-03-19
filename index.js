c4d24f7a4f65dbd43ecd2b6302d28bbd37ac25f7
Han Pham <41082484+hanpm@users.noreply.github.com>
2024-03-18 23:39:26 -0700

    editing the comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Calling the lambda function 
+// Lambda function trigger
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://dafbprumukerwlot2pqi3jg7o40orqxd.lambda-url.us-west-1.on.aws/");
