820f833b95f9f824399ed0b95b73269a696265fd
Han Pham <41082484+hanpm@users.noreply.github.com>
2024-03-18 23:50:35 -0700

    Editing the html, js, css

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://dafbprumukerwlot2pqi3jg7o40orqxd.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data};`
+    counter.innerHTML = `${data}`;
 }
 updateCounter();
